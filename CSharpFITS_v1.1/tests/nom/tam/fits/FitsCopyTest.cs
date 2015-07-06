fd9a5162fc9fa3ac832f03d62840bdfb7643e6ce
rwg0 <robin.glover@workshare.com>
2015-07-06 17:24:15 +0100

    As per source distribution with binary folders removed

---
+++ CSharpFITS_v1.1/tests/nom/tam/fits/FitsCopyTest.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using NUnit.Framework;
+
+using nom.tam.fits;
+using nom.tam.image;
+using nom.tam.util;
+
+namespace nom.tam.fits
+{
+    [TestFixture]
+    public class FitsCopyTester
+    {
+        [Test]
+        public void TestFitsCopy( /*String[] args*/ )
+        {
+
+            String file = "E:\\CSharpFITSIO\\AISCHV3_228_13637_0001_sv09-fd-int.fits" /*args[0]*/;
+
+            Fits f = new Fits(file);
+            int i = 0;
+            BasicHDU h;
+
+            do
+            {
+                h = f.ReadHDU();
+                if (h != null)
+                {
+                    if (i == 0)
+                    {
+                        System.Console.Out.WriteLine("\n\nPrimary header:\n");
+                    }
+                    else
+                    {
+                        System.Console.Out.WriteLine("\n\nExtension " + i + ":\n");
+                    }
+                    i += 1;
+                    h.Info();
+                }
+            } while (h != null);
+
+            BufferedFile bf = new BufferedFile("gbfits3.fits" /*args[1]*/, FileAccess.ReadWrite, FileShare.ReadWrite);
+            f.Write(bf);
+            bf.Close();
+
+        }
+    }
+}
